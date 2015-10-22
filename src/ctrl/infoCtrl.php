2551195dd9d07d7b10e9f0ce1c16abcf93b05175
Julien VIDAL <jvidal@teads.tv>
2015-10-23 00:19:26 +0200

    Implement info input script filter

--- src/ctrl/infoCtrl.php
+++ src/ctrl/infoCtrl.php
@@ -4,41 +4,51 @@ namespace JenkinsHelper;
 
 class infoCtrl extends BaseCtrl{
 
-	private $infoData = array(
-		'hostname' => array(
-			'label' => 'Set your base hostname (ex. jenkins.mydomain.com) {{query}}'
-		),
-		'username' => array(
-			'label' => 'Set your username to {{query}}'
-		),
-		'password' => array(
-			'label' => 'Set your password to {{query}}'
-		),
-		'protocol' => array(
-			'label' => 'Set your protocol (default is https) {{query}}'
-		),
-		'token' => array(
-			'label' => 'Set your jenkins api token {{query}}'
-		)
-	);
-
-	private $defaultResult = array(
-		'icon' 	=> 'icon.png',
-		'valid' => 'yes',
-		'autocomplete' => '{{query}}'
-	);
+	private $infoData = null; 
+	private $defaultResult = null;
 
 	function __construct($query, $action, $workflows){
 		parent::__construct($query);
+		$this->initInfoData();
 		$this->{$action."Action"}();
 	}
 
+	private function initInfoData(){
+		$this->infoData 			= new \stdClass();
+		$this->defaultResult 	= new \stdClass();
+
+		$this->infoData->hostname = array(
+			'label' => "Set your base hostname (ex. jenkins.mydomain.com) $this->query"
+		);
+		$this->infoData->username = array(
+			'label' => "Set your username to $this->query"
+		);
+		$this->infoData->password = array(
+			'label' => "Set your password to $this->query"
+		);
+		$this->infoData->protocol = array(
+			'label' => "Set your protocol (default is https) $this->query"
+		);
+		$this->infoData->token = array(
+			'label' => "Set your jenkins api token $this->query"
+		);
+
+		$this->defaultResult->icon = 'icon.png';
+		$this->defaultResult->valid = 'yes';
+		$this->defaultResult->autocomplete = $this->query;
+	}
+
 	private function inputAction(){
-		$this->workflowH->result( 'hostname', "$query  hostname", "$query", 'Set your base hostname (ex. jenkins.mydomain.com) '.$this->query, 'icon.png', 'yes', "$query" );
-		$this->workflowH->result( 'username', "$query  username", "$query", 'Set your username to '.$this->query, 'icon.png', 'yes', "$query" );
-		$this->workflowH->result( 'password', "$query  password", "$query", 'Set your password to '.$this->query, 'icon.png', 'yes', "$query" );
-		$this->workflowH->result( 'protocol', "$query  protocol", "$query", 'Set your protocol (default is https) '.$this->query, 'icon.png', 'yes', "$query" );
-		$this->workflowH->result( 'token', "$query  token", "$query", 'Set your jenkins api token '.$this->query, 'icon.png', 'yes', "$query" );
+		foreach ($this->infoData as $key => $value) {
+			$this->workflowH->result( 
+				$key, 
+				"$this->query  $key", 
+				"$this->query", 
+				$value['label'], 
+				$this->defaultData->icon, 
+				$this->defaultData->valid, 
+				$this->defaultData->autocomplete );
+		}
 	}
 
 	private function outputAction(){
